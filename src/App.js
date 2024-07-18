1bd208375731527474c8f191da3acce931d3e80a
rhrits <hritik.hritiksingh@gmail.com>
2024-07-19 02:19:41 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import the custom CSS file
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Infinite scroll your Google search result page</h1>
+        <div className="made-by">
+          <p>Made by Hritik</p>
+          <div className="github-container">
+            <p className="heart">❤️</p>
+            <a
+              href="https://github.com/rhrits/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="github-button"
+            >
+              <img src="/github-icon.png" alt="GitHub" className="github-icon" />
+            </a>
+            <p className="check-my-github">Check my GitHub</p>
+          </div>
+        </div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/google-infinite-scroll-extension.zip"
+          download
+          className="download-button"
         >
-          Learn React
+          Download Extension
         </a>
+        <div className="instructions">
+          <h2>Steps to use this extension:</h2>
+          <ol>
+            <li>Click the "Download Extension" button.</li>
+            <li>Unzip the downloaded file.</li>
+            <li>Open Chrome and go to <code>chrome://extensions/</code>.</li>
+            <li>Enable "Developer mode" by clicking the toggle switch at the top right.</li>
+            <li>Click the "Load unpacked" button.</li>
+            <li>Select the unzipped folder containing your extension files.</li>
+            <li>Now refresh the page.</li>
+            <li>You are ready to use the extension!</li>
+          </ol>
+        </div>
       </header>
     </div>
   );
